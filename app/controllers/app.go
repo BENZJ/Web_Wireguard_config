c4cabe68c5f877414a7578a65e58824b1a632f9b
BENZJ <jbcbenz@gmail.com>
2020-10-05 09:23:29 +0800

    简易版本实现修改文件接下来放到linux环境中开始实验部署

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,9 +2,9 @@ package controllers
 
 import (
 	"benzj/wgconfig/app/models"
+	"bufio"
 	"fmt"
 	"github.com/revel/revel"
-	"io/ioutil"
 	"os"
 )
 
@@ -12,15 +12,10 @@ type App struct {
 	*revel.Controller
 }
 
+var path_userconf string = "/etc/wireguard/user.conf"
+
 func (c App) Index() revel.Result {
-	file, err := os.Open("/Users/benz/go/src/benzj/wgconfig/wg0.conf")
-	if err != nil {
-		fmt.Println("读取失败")
-		panic(err)
-	}
-	defer file.Close()
-	content, err := ioutil.ReadAll(file)
-	fmt.Println(string(content))
+
 	return c.Render()
 }
 
@@ -28,7 +23,19 @@ func (c App) Login(myName string, myPassword string) revel.Result {
 	xxuser := new(models.User)
 	xxuser.Username = "hello"
 
-	if myName == "benz" && myPassword == "jbcbenz" {
+	file, err := os.Open(path_userconf)
+	if err != nil {
+		fmt.Println("读取失败")
+		panic(err)
+	}
+	defer file.Close()
+	br := bufio.NewReader(file)
+	a, _, _ := br.ReadLine()
+	username := string(a)
+	a, _, _ = br.ReadLine()
+	password := string(a)
+
+	if myName == username && myPassword == password {
 		c.Session["user"] = myName
 		c.Session.SetDefaultExpiration()
 		c.Flash.Success("Welcome, " + myName)
--- app/controllers/wgconfig.go
+++ app/controllers/wgconfig.go
@@ -2,15 +2,55 @@ package controllers
 
 import (
 	// "benzj/wgconfig/app/models"
+	"fmt"
 	"github.com/revel/revel"
+	"io/ioutil"
+	"os"
+	"os/exec"
 )
 
 type WgConfig struct {
 	*revel.Controller
 }
 
+var path_wgconf string = "/etc/wireguard/wg0.conf"
+
+// var path_wgconf string = "/etc/wireguard/wg0.conf"
+
 func (c WgConfig) Index() revel.Result {
-	return c.Render()
+	data, err := ioutil.ReadFile(path_wgconf)
+	if err != nil {
+		fmt.Println("File reading error", err)
+		panic(err)
+	}
+	wg0conf := string(data)
+	return c.Render(wg0conf)
+}
+
+func (c WgConfig) SaveAndReastart(wgconftex string) revel.Result {
+	cmd := exec.Command("wg-quick", "down", "wg0")
+	cmd.Run()
+	f, err := os.Create(path_wgconf)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	_, err = f.WriteString(wgconftex)
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		panic(err)
+	}
+	fmt.Println("写入成功")
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	cmd = exec.Command("wg-quick", "up", "wg0")
+	cmd.Run()
+	c.Flash.Success("重启Wireguard完成")
+	return c.Redirect(WgConfig.Index)
 }
 
 func (c WgConfig) checkUser() revel.Result {
